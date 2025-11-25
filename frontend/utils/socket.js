0b301b2fee5c8f09d914059f91c06dfe06f6f9aa
KhalidTheCoder <khalidahsan2407@gmail.com>
2025-11-25 14:00:00 +0600

    feat(web): add socket.io-client utility
    Creates a singleton socket connection to the backend.
    Exported for use in Chat and notification components.

---
+++ frontend/utils/socket.js
@@ -0,0 +1,29 @@
+/**
+ * @file socket.js
+ * @description Singleton Socket.io-client connection for real-time communication with the Cresify backend.
+ * @module utils/socket
+ */
+
+import { io } from 'socket.io-client';
+import { root_url } from './utils';
+
+let socket;
+
+export const initSocket = (userId) => {
+  if (!socket) {
+    socket = io(root_url, {
+      query: { userId },
+    });
+
+    socket.on('connect', () => {
+      console.log('Connected to socket server:', socket.id);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from socket server');
+    });
+  }
+  return socket;
+};
+
+export const getSocket = () => socket;
